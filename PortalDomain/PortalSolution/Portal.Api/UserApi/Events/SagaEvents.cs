64f0fa00ae6cf5ea745989093ade9b78b3ab42f4
Jeffry Gonzalez <jeff@hypertheory.com>
2023-10-18 16:13:30 -0400

    :shipit:

--- PortalDomain/PortalSolution/Portal.Api/UserApi/Events/SagaEvents.cs
+++ PortalDomain/PortalSolution/Portal.Api/UserApi/Events/SagaEvents.cs
@@ -1,5 +1,21 @@
-﻿namespace Portal.Api.UserApi.Events;
+﻿using Wolverine;
 
-public class SagaEvents
-{
-}
+namespace Portal.Api.UserApi.Events;
+/*### `company.com.portal.user-issue.annotated`
+
+### `company.com.portal.user-issue.rejected`
+
+### `company.com.portal.user-issue.created`
+*/
+
+public record UserIssueAnnotated(string message);
+public record UserIssueRejected();
+
+public record IssueCreated();
+
+
+public record AnnotationAdded(string Note);
+
+public record UserIssueTimeOut(string Id) : TimeoutMessage(TimeSpan.FromMinutes(10));
+
+public record SagaStarted(string Id);
